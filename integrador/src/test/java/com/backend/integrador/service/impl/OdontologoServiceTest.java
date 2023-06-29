48ae557654056c9eb292eb33aba3b9847ca1c78d
PachoSur <pacho.baires@gmail.com>
2023-06-28 23:38:55 -0300

    OdontologoServiceTest completado.

--- integrador/src/test/java/com/backend/integrador/service/impl/OdontologoServiceTest.java
+++ integrador/src/test/java/com/backend/integrador/service/impl/OdontologoServiceTest.java
@@ -1,9 +1,14 @@
 package com.backend.integrador.service.impl;
 
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import com.backend.integrador.dto.OdontologoDto;
+import com.backend.integrador.entity.Odontologo;
+import com.backend.integrador.exception.BadRequestException;
+import com.backend.integrador.exception.ResourceNotFoundException;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.validation.ConstraintViolationException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,8 +16,25 @@ import static org.junit.jupiter.api.Assertions.*;
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class OdontologoServiceTest {
 
+    @Autowired
+    private OdontologoService odontologoService;
+
+
     @Test
-    void agregarOdontologo() {
+    @Order(1)
+    void deberíaAgregarOdontologo() throws BadRequestException {
+
+        Odontologo odontologoAAgregar = new Odontologo("Juan", "Gallego", "UY-12345678");
+        OdontologoDto odontologoDto = odontologoService.agregarOdontologo(odontologoAAgregar);
+
+        Assertions.assertNotNull(odontologoDto);
+    }
+
+    @Test
+    @Order(2)
+    void cuandoNoSeCumplaElFormatoDeMatricula_noDeberiaInsertarElOdontologo() {
+        Odontologo odontologo = new Odontologo("3433333", "patricia", "Lopez");
+        Assertions.assertThrows(ConstraintViolationException.class, () -> odontologoService.agregarOdontologo(odontologo));
     }
 
     @Test
@@ -20,6 +42,9 @@ class OdontologoServiceTest {
     }
 
     @Test
-    void buscarOdontologoPorId() {
+    @Order(4)
+    void deberiaEliminarElOdontologoId1() throws ResourceNotFoundException {
+        odontologoService.eliminarOdontologo((long) 1);
+        Assertions.assertThrows(ResourceNotFoundException.class, () -> odontologoService.eliminarOdontologo(1L));
     }
 }
